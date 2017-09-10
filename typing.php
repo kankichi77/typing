c85d47389b1b9ed49cb586cec6865ee156b5c678
Kan Hayashi <kan@kan-mba11a.local>
2017-09-10 17:13:22 +0900

    add files

--- typing.php
+++ typing.php
@@ -3,7 +3,16 @@
 
 ?>
 <HTML>
-<HEAD><TITLE>Emi's Typing Game</TITLE></HEAD>
+<HEAD>
+  <TITLE>Emi's Typing Game</TITLE></HEAD>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css">
+  <title>KKB</title>
+  <script src="jquery-3.2.1.js"></script>
+  <link rel="stylesheet" href="jquery-ui.min.css">
+  <script src="jquery-ui.min.js"></script>
 <BODY>
 
 
